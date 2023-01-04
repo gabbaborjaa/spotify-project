e687305d17124c74f908c60ab550040fab752661
gabbaborjaa <gab.borja26@gmail.com>
2023-01-03 22:23:22 -0500

    first

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const options = {
     }
 };
 
-fetch('https://spotify23.p.rapidapi.com/search/?q=brockhampton&type=multi&offset=0&limit=10&numberOfTopResults=5', options)
+const search = document.getElementById('query');
+
+
+
+fetch(`https://spotify23.p.rapidapi.com/search/?q=${search}&type=multi&offset=0&limit=10&numberOfTopResults=5`, options)
     .then(response => response.json())
     .then(response => console.log(response))
     .catch(err => console.error(err));
